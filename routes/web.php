995060ab62a6c779be7ebb947e147b721d4ac79e
Vipu <lessandsass@gmail.com>
2025-01-17 19:28:24 +0530

    Admin Login Functionality
    - The AdminLoginController was created inside the views\admin directory using the artisan command
    - The index method was added in the AdminLoginController class
    - The admin-login route was created and named as admin.login in the web.php file
    - Make sure you have imported the AdminLoginController class in the routes file.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\admin\AdminLoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/admin/login', [AdminLoginController::class, 'index'])->name('admin.login');
+
+
+
