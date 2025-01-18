e33cc80de48af820cefd119817b1899128b3b371
Vipu <lessandsass@gmail.com>
2025-01-18 19:24:18 +0530

    Defining Custom middlewares
    - Defining AdminAuthenticate and AdminRedirectIfAuthenticated middlewares
    - The AdminAuthenticate middleware was created using artisan command
    - The AdminRedirectIfAuthenticated middleware was created using artisan command
    - The logic was added for both middlewares

---
+++ app/Http/Middleware/AdminAuthenticate.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdminAuthenticate
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (auth()->guard('admin')->check()) {
+            return $next($request);
+        } else {
+            return auth()->shouldUse('admin');
+        }
+    }
+}
