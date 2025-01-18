d952e1037694cfb2ad956974c205da6de1e5c59e
Vipu <lessandsass@gmail.com>
2025-01-18 16:46:32 +0530

    Adding roles to the user table
    - The add_user_roles migration file had been created to implement the roles and permission functionality.
    - by adding the table flag we can pass the name of the table to be altered by using the migration.

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -6,7 +6,7 @@
     <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
         <x-logo />
 
-        <span class="ml-2 pl-2">Blog-Vipu</span>
+        <span class="ml-2 pl-2">Admin Login</span>
     </a>
 
     <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
