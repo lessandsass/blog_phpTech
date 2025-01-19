f46db54ed8f5ae8112512d9e96b952c1ca3091bc
Vipu <lessandsass@gmail.com>
2025-01-19 06:15:06 +0530

    Session variable
    - The value was added in the old input field

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -20,7 +20,13 @@
                 @csrf
                 <div>
                     <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                    <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com">
+                    <input
+                        type="email"
+                        name="email"
+                        id="email"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com"
+                        value="{{ old('email') }}"
+                    >
 
                     @error('email')
                         <p class="mt-2 text-sm text-red-600 dark:text-red-500">
