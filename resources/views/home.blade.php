ed165016143580ad03221322ddf6bdecea097cda
Vipu <lessandsass@gmail.com>
2025-02-09 18:06:01 +0530

    The new admin dashboard
    - The new admin dashboard mark up was added
    - The app.blade.php layout file was pulled in the welcome blade file.
    - The welcome view was replaced by the home view that was newly created.

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">Home</h1>
+@endsection
+
+
--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\admin\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 })->name('home');
 
 Route::group(['prefix' => 'admin'], function() {
